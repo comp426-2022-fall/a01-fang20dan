ff10fc2570998537a67ec07c9b7bf6edfa22f8b0
Daniel Fang <fang20d@email.unc.edu>
2022-08-30 17:28:09 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 3. end with the data that you are reading in from ./public/index.html.
 	const server = http.createServer((req, res) => {
 		res.statusCode = 200
-		res.setHeader('Content-Type', 'text.html')
+		res.setHeader('Content-Type', 'text/html')
 		res.end(data)
 	})
 
